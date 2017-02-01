ce516be9c8d15ccea46c77aeace0818c66aeeb6c
yunjey <yunjey47@naver.com>
2017-02-01 12:23:54 +0900

    initial commit

---
+++ model.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+class AdaptiveAttentionModel(object):
+    """Adaptive Attention Model
+    """
+    def __init__(self):
+        pass
+        
+   
+    def build_model(self):
+        pass
